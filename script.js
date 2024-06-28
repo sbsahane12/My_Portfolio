f6cba700d055f2c6301438cc3b80b2405948b61a
Sagar Bhausaheb Sahane <106218600+sbsahane12@users.noreply.github.com>
2024-06-28 11:09:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Developer", "Freelancer"],
+    strings: ["Developer", "Freelancer","Project Manger"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
